ddcd6f098dd9aa78993f23262706f832461b0959
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-24 15:50:57 -0600

    injectar http module

--- src/app/paises/paises.module.ts
+++ src/app/paises/paises.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { SelectorComponent } from './pages/selector/selector.component';
 import { PaisesRoutingModule } from './paises-routing.module';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
     declarations: [
         SelectorComponent
     ],
     imports: [
-        PaisesRoutingModule
+        PaisesRoutingModule,
+        HttpClientModule
     ],
     exports: [
         RouterModule
--- src/app/paises/services/pais.service.ts
+++ src/app/paises/services/pais.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
     providedIn: 'root'
 })
-export class PaisService { }
+export class PaisService {
+
+    constructor(private httpClient: HttpClient) { }
+
+    
+}
